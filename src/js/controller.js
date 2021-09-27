eb010f23011de24dc093ee5e31746f4cf15da7bc
Sylvester Ugwoke <si.ugwoke@gmail.com>
2021-09-27 10:47:33 +0100

    Completed styling. Separated the details into a separate page

---
+++ src/js/controller.js
@@ -0,0 +1,11 @@
+// const btn = document.querySelector('.filter__btn');
+// const regions = document.querySelector('.filter__regions');
+// const page = document.documentElement;
+// const filteterIcon = document.querySelector('.filter__icon');
+
+// btn.addEventListener('click', function (e) {
+//   console.log(regions);
+//   console.log(e.target);
+//   regions.classList.toggle('filter__regions--expanded');
+//   filteterIcon.classList.toggle('filter__icon-rotate');
+// });
